9c975616e752a29f7bc4418aa9cc90fd11831eab
James Jensen <jnjensen@live.com>
2020-02-04 02:43:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,3 +1,6 @@
+var chocolateBars = [
+  "snickers", "hundred grand", "kitkat", "skittles"]
+
 function destructivelyAddElementToBeginningOfArray (array, element){
   array.unshift(element)
   return array
